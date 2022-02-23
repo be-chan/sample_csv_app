7266a4128cd34a7bc45eecef6987e8cb2e0ecef3
be-chan <70454487+be-chan@users.noreply.github.com>
2022-02-24 00:18:08 +0900

    gemなし検索機能
    ### gemなし検索機能
    - テキストと日付検索

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,29 @@
 class PostsController < ApplicationController
+
   def index
+    @post = Post.new
+
+    # 検索用
+    if params[:search].present? 
+      @posts = Post.where("title LIKE ?", "%#{params[:search]}%")
+    end
+    if params[:date_search].present?
+      @posts = Post.where("contract = ?", params[:date_search])
+    end
+    p @posts
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_index_path
+    else
+      render posts_index_path
+    end
+  end
+
+  private
+  def posts_params
+    params.require(:post).permit(:title, :contract)
   end
 end
