d2faaecb8a911bd3c072b59eba254207486a15c2
be-chan <70454487+be-chan@users.noreply.github.com>
2022-03-16 07:17:13 +0900

    新規登録と確認画面

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,10 @@ Rails.application.routes.draw do
   post 'articles/create'
   
   get 'api/request', to: 'api_requests#connection'
+
+  get 'reserves/new' # 入力画面
+  post 'reserves/confirm' # 確認画面
+  post 'reserves/back' # 確認画面から「入力画面に戻る」をクリックしたとき
+  post 'reserves/complete' # 完了画面
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_221748) do
+ActiveRecord::Schema.define(version: 2022_03_15_215505) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2022_02_24_221748) do
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.date "contract"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reserves", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "reserce_date"
+    t.string "name"
+    t.string "note"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
